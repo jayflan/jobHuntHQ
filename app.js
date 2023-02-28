525a86b3dd7f3d5ec6f3b10492ace6e23635d8eb
James Flanary <jflanmacpro@Jamess-MacBook-Pro.local>
2023-02-28 09:19:45 -0500

    deploy testing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // const { Cheerio, load } = require("cheerio");
 const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-require('dotenv').config();
+// const morgan = require("morgan");
+// const path = require("path");
+// require('dotenv').config();
 
 const app = express();
 
