6d6747ee081ebb2c33b34e9046389d2cb2dc4945
James Flanary <jflanmacpro@Jamess-MacBook-Pro.local>
2023-03-02 19:15:07 -0500

    new routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 module.exports = router;
 
-router.use("/jobs", require("./jobs"));
+// router.use("/jobs", require("./jobs"));
 router.use("/test", require("./test"));
+router.use("/webscraper", require("./webScraper"));
+router.use("/search", require("./search.js"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
